a9b4d795e9d1641ed2c7203bacd0dd9506556805
Simone Taeggi <startae14@gmail.com>
2026-02-28 01:21:18 +0100

    01 Creating a Component

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,16 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>Aleph</li>
+      <li>Back to the future</li>
+      <li>Drumshow</li>
+    </ol>
   `,
-  styles: ``,
+  styles: `
+    ol {
+      list-style-type: upper-roman;
+    }
+  `,
 })
 export class AppComponent {}
