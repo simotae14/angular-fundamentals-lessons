ff91db0634c271a6b92f55f8ecf34f811cdafad4
Simone Taeggi <startae14@gmail.com>
2026-04-21 09:12:50 +0200

    14 Dependency Injection DI

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './data';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  imports: [UserInfoComponent],
+  template: `
+    <h1>User Listing</h1>
+    @for (user of userData; track user.id) {
+      <app-user-info [user]="user" />
+    }
+  `,
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  // this one is a Singleton instance since it is spread in the root component
+  userService = inject(UserService);
+  userData: User[] = [];
+
+  async ngOnInit(): Promise<void> {
+    const data = await this.userService.getUserData();
+    this.userData = data;
+  }
 }
--- projects/11-dependency-injection/src/app/user.service.ts
+++ projects/11-dependency-injection/src/app/user.service.ts
@@ -1,4 +1,9 @@
+import { Injectable } from '@angular/core';
 import { data, User } from './data';
+
+@Injectable({
+  providedIn: 'root',
+})
 
 export class UserService {
   private userData: User[] = data;
